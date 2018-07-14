70b8ec339ae3ed176743a5a9e64e562ca14fe270
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:44:08 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,10 @@
+module com.jwebmp.plugins.bootstraptoggle {
+	exports com.jwebmp.plugins.bootstraptoggle;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires com.jwebmp.plugins.bootstrap4;
+	requires java.validation;
+}

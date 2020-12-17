ca2b98f36cc999523a81c0c081bdb6dae35cd53f
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:16 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/bs4/toggle/BSSwitch4PageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/bs4/toggle/BSSwitch4PageConfigurator.java
@@ -75,7 +75,7 @@ public class BSSwitch4PageConfigurator
 	
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
